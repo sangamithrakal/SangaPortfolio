72979c8821ba828729900fbbfa033687361e5314
Nishant Neeraj <nishant.neeraj@gmail.com>
2018-07-05 11:26:01 +0530

    YES!

--- src/index.js
+++ src/index.js
@@ -1,8 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import {Pager} from "react-bootstrap";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import ReactPageScroller from "react-page-scroller";
+import FirstComponent from "./FirstComponent";
+import SecondComponent from "./SecondComponent";
+import ThirdComponent from "./ThirdComponent";
+import FourthComponent from "./FourthComponent";
+import FifthComponent from "./FifthComponent";
+
+import "./index.css";
+
+class Demo extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {currentPage: 1};
+        this._pageScroller = null;
+    }
+
+    goToPage = (eventKey) => {
+        this._pageScroller.goToPage(eventKey);
+    };
+
+    pageOnChange = (number) => {
+        this.setState({currentPage: number});
+    };
+
+    getPagesNumbers = () => {
+
+        const pageNumbers = [];
+
+        for (let i = 1; i <= 5; i++) {
+            pageNumbers.push(
+                <Pager.Item key={i} eventKey={i - 1} onSelect={this.goToPage}>{i}</Pager.Item>
+            )
+        }
+
+        return [...pageNumbers];
+    };
+
+    render() {
+
+        const pagesNumbers = this.getPagesNumbers();
+
+        return <React.Fragment>
+            <ReactPageScroller ref={c => this._pageScroller = c} pageOnChange={this.pageOnChange}>
+                <FirstComponent/>
+                <SecondComponent/>
+                <ThirdComponent/>
+                <FourthComponent goToPage={this.goToPage}/>
+                <FifthComponent/>
+            </ReactPageScroller>
+            <Pager className="pagination-additional-class" bsSize="large">
+                {pagesNumbers}
+            </Pager>
+        </React.Fragment>
+    }
+}
+
+ReactDOM.render(<Demo/>, document.getElementById("root"));
