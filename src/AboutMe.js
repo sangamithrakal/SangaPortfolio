e841a0c487a20911abff700a087b510872de49dd
Nishant Neeraj <nishant.neeraj@gmail.com>
2018-07-15 21:28:09 +0530

    additions

---
+++ src/AboutMe.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import WorkIcon from 'react-icons/lib/fa/beer'
+import Crown from './CrownIcon'
+import StarIcon from 'react-icons/lib/fa/star'
+import SchoolIcon from 'react-icons/lib/fa/anchor'
+import './custom.css'
+
+class AboutMe extends Component {
+    render() {
+      return (
+        <div className="AboutMe"> 
+              ABOUT ME MORE DETAIL ABOUT ME MORE DETAILABOUT ME MORE DETAILABOUT ME MORE DETAILABOUT ME MORE DETAILABOUT ME MORE DETAILABOUT ME MORE DETAILABOUT ME
+               MORE DETAILABOUT ME MORE DETAILABOUT ME MORE DETAILABOUT ME MORE DETAIL ABOUT ME MORE DETAILABOUT ME MORE DETAIL
+          </div>
+
+    );
+}
+}
+
+export default AboutMe;
---
+++ src/Skills.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import WorkIcon from 'react-icons/lib/fa/beer'
+import Crown from './CrownIcon'
+import StarIcon from 'react-icons/lib/fa/star'
+import SchoolIcon from 'react-icons/lib/fa/anchor'
+import './custom.css'
+
+class Skills extends Component {
+    render() {
+      return (
+        <div className="AboutMe"> 
+              SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS 
+              SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS 
+              SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS 
+              SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS SKILLS 
+          </div>
+
+    );
+}
+}
+
+export default Skills;
