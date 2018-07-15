483de744428bf211c6936e903b9d8a169f99790b
Nishant Neeraj <nishant.neeraj@gmail.com>
2018-07-15 22:20:34 +0530

    additions

---
+++ src/Hobbies.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import WorkIcon from 'react-icons/lib/fa/beer'
+import Crown from './CrownIcon'
+import StarIcon from 'react-icons/lib/fa/star'
+import SchoolIcon from 'react-icons/lib/fa/anchor'
+import './custom.css'
+
+class Hobbies extends Component {
+    render() {
+      return (
+        <div className="AboutMe"> 
+            HOBBIES HOBBIES HOBBIES HOBBIES HOBBIES HOBBIES HOBBIES HOBBIES HOBBIES HOBBIES HOBBIES HOBBIES HOBBIES 
+            HOBBIES HOBBIES HOBBIES HOBBIES HOBBIES HOBBIES HOBBIES HOBBIES HOBBIES HOBBIES HOBBIES HOBBIES HOBBIES 
+            HOBBIES HOBBIES HOBBIES HOBBIES HOBBIES HOBBIES HOBBIES HOBBIES HOBBIES HOBBIES HOBBIES HOBBIES HOBBIES 
+        </div>
+
+    );
+}
+}
+
+export default Hobbies;
